315782165bdb0fd2bdc294df8549e3d3897725ea
Simone Cernuschi <cersimo97@gmail.com>
2022-09-12 21:17:12 +0200

    Add encode test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,16 @@
-const { decodeCF } = require('../src')
+const { decodeCF, encodeCF } = require('../src')
 
 describe('Main functions', () => {
-  it('Should encode a CF', () => {})
+  it('Should encode a CF', () => {
+    const firstName = 'simone'
+    const lastName = 'cernuschi'
+    const birthdate = 873669600000
+    const isFemale = false
+    const birthCity = 'treviglio'
+    expect(
+      encodeCF({ firstName, lastName, birthdate, birthCity, isFemale })
+    ).toBe('CRNSMN97P08L400G')
+  })
   it('Should decode a CF', () => {
     const cf = 'CRNSMN97P08L400G'
     expect(decodeCF(cf)).toStrictEqual({
